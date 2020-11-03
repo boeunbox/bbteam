2d2c6267dae2920ec4557e2cb5f33c121d420238
seongyeol-park-arch <67372257+seongyeol-park-arch@users.noreply.github.com>
2020-11-03 22:21:15 +0900

    이거상한데

--- boeunbox/signupapp/models.py
+++ boeunbox/signupapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class customer_information(models.Model):
+    email = models.CharField('이메일',max_length=200)
+    password = models.TextField('암호',blank=True)
+    created_at = models.DateTimeField('생성날짜', auto_now_add=True) #생성될때 픽스됨
+    modified_at = models.DateTimeField('수정날짜', auto_now=True)#수정할때마다 바뀜
+
+    def __str__(self):
+        return self.title
