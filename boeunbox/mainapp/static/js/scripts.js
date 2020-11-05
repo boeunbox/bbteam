5d90b54c59d3a37a0fb0d34bf008c6447bba72f7
Mina <mina9301@gmail.com>
2020-11-06 01:23:27 +0900

    네비게이션바 수정

--- boeunbox/mainapp/static/js/scripts.js
+++ boeunbox/mainapp/static/js/scripts.js
@@ -40,7 +40,8 @@
     var navbarCollapse = function () {
         if ($("#mainNav").offset().top > 100) {
             $("#mainNav").addClass("navbar-shrink");
-        } else {
+        }
+        else {
             $("#mainNav").removeClass("navbar-shrink");
         }
     };
