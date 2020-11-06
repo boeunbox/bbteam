ce9bfc161b2237027b4ba423c3c976bd8bbf88a5
Mina <mina9301@gmail.com>
2020-11-06 21:54:27 +0900

    네비게이션바 전체 수정

--- boeunbox/mainapp/static/js/scripts.js
+++ boeunbox/mainapp/static/js/scripts.js
@@ -1,52 +1,20 @@
-(function ($) {
-    "use strict"; // Start of use strict
+// 메뉴 펼치기
+const menuicon = document.querySelector('.menu-icon');
+const menu = document.querySelector('.menu');
 
-    // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-        if (
-            location.pathname.replace(/^\//, "") ==
-            this.pathname.replace(/^\//, "") &&
-            location.hostname == this.hostname
-        ) {
-            var target = $(this.hash);
-            target = target.length
-                ? target
-                : $("[name=" + this.hash.slice(1) + "]");
-            if (target.length) {
-                $("html, body").animate(
-                    {
-                        scrollTop: target.offset().top - 70,
-                    },
-                    1000,
-                    "easeInOutExpo"
-                );
-                return false;
-            }
-        }
-    });
+menuicon.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
 
-    // Closes responsive menu when a scroll trigger link is clicked
-    $(".js-scroll-trigger").click(function () {
-        $(".navbar-collapse").collapse("hide");
-    });
 
-    // Activate scrollspy to add active class to navbar items on scroll
-    $("body").scrollspy({
-        target: "#mainNav",
-        offset: 100,
-    });
-
-    // Collapse Navbar
-    var navbarCollapse = function () {
-        if ($("#mainNav").offset().top > 100) {
-            $("#mainNav").addClass("navbar-shrink");
+// Scrolling Effect
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 150) {
+            $('.navbar').addClass('black');
         }
         else {
-            $("#mainNav").removeClass("navbar-shrink");
+            $('.navbar').removeClass('black');
         }
-    };
-    // Collapse now if page is not at top
-    navbarCollapse();
-    // Collapse the navbar when page is scrolled
-    $(window).scroll(navbarCollapse);
-})(jQuery); // End of use strict
+    });
+});
