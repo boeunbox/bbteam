49f1c30cfd43417b497fc1c647c0bad9c6876767
Mina <mina9301@gmail.com>
2020-11-07 20:30:54 +0900

    base 관련 파일 전체 수정

--- boeunbox/mainapp/static/js/scripts.js
+++ boeunbox/mainapp/static/js/scripts.js
@@ -1,20 +1,40 @@
 // 메뉴 펼치기
-const menuicon = document.querySelector('.menu-icon');
-const menu = document.querySelector('.menu');
+// const menuicon = document.querySelector('.menu-icon');
+// const menu = document.querySelector('.menu');
 
-menuicon.addEventListener('click', () => {
-    menu.classList.toggle('active');
+// menuicon.addEventListener('click', () => {
+//     menu.classList.toggle('active');
+// });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Transition effect for navbar 
+    window.addEventListener('scroll', function (e) {
+        // checks if window is scrolled more than 500px, adds/removes solid class
+        if (window.scrollY > 100) {
+            document.getElementById("id_nav").classList.add("black")
+        } else {
+            document.getElementById("id_nav").classList.remove('black');
+        }
+    });
 });
 
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myCollapsed").classList.toggle("active");
+}
 
-// Scrolling Effect
-// $(document).ready(function(){
-// $(window).on("scroll", function () {
-//     if ($(this).scrollTop()) {
-//         $(".navbar").addClass("black");
-//     }
-//     else {
-//         $(".navbar").removeClass("black");
-
-//     });
-// });
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.btn-collapse')) {
+        var collapse = document.getElementsByClassName("collapsed-content");
+        var i;
+        for (i = 0; i < collapse.length; i++) {
+            var collapseNav = collapse[i];
+            if (collapseNav.classList.add('active')) {
+                collapseNav.classList.remove('active');
+            }
+        }
+    }
+}
