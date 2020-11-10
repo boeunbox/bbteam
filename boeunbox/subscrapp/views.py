a5fd0bbd534e35218de53540d0ed929b1da2ff1d
홍정빈 <hjb3880@likelion.org>
2020-11-10 22:49:00 +0900

    가격표 미디어 쿼리 적용

--- boeunbox/subscrapp/views.py
+++ boeunbox/subscrapp/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def subscr(request):
     return render(request, 'subscr.html')
+
+
+# def login(request):
+#     context = {}
+#     return render(request, 'login.html', context)
