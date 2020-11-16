8c6cd0723260e27df5ce0510d8f47c66c1ac24d4
Mina <mina9301@gmail.com>
2020-11-17 05:12:44 +0900

    cscenterapp과 signupapp migration 진행

--- boeunbox/cscenterapp/urls.py
+++ boeunbox/cscenterapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import cscenter,create
 urlpatterns = [
     path('create/', create, name="create"),
-    path('', cscenter, name="cscenter"),
+    path('cscenter/', cscenter, name="cscenter"),
     # path('detail/<int:detail_id>/', detail, name="detail"),
     # path('update/<int:update_id>/', update, name="update"),
     # path('delete/<int:delete_id>/', delete, name="delete"),
